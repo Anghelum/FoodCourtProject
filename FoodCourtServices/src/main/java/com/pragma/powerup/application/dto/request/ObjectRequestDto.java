a87b3c7b96fa9f541a58558658380e49ee500572
Anghelum <anghelum3013@gmail.com>
2023-03-17 18:45:57 -0500

    Initial commit

---
+++ FoodCourtServices/src/main/java/com/pragma/powerup/application/dto/request/ObjectRequestDto.java
@@ -0,0 +1,12 @@
+package com.pragma.powerup.application.dto.request;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class ObjectRequestDto {
+    private String name;
+}

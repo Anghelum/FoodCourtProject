2b57a8dbba59afd368a71fccdf02376993ef69a4
Anghelum <anghelum3013@gmail.com>
2023-03-17 18:48:36 -0500

    Prueba

--- FoodCourtServices/src/main/java/com/pragma/powerup/application/dto/request/ObjectRequestDto.java
+++ FoodCourtServices/src/main/java/com/pragma/powerup/application/dto/request/ObjectRequestDto.java
@@ -1,12 +1,10 @@
 package com.pragma.powerup.application.dto.request;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Data
 public class ObjectRequestDto {
     private String name;
 }
